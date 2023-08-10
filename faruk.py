9b0ea7d0d5b0d36d2e113b5262c9aef6511867e3
FarukPhytonPro <140546192+FarukPhytonPro@users.noreply.github.com>
2023-08-10 20:08:42 +0300

    Update faruk.py

--- faruk.py
+++ faruk.py
@@ -1,69 +1,88 @@
 import discord
+from discord.ext import commands
+from bot_mantik import gen_pass
 import random
-import time
+a = random.randint(1, 2)
 
-# ayricaliklar (intents) deðiþkeni botun ayrýcalýklarýný depolayacak
 intents = discord.Intents.default()
-# Mesajlarý okuma ayrýcalýðýný etkinleþtirelim
 intents.message_content = True
-# client (istemci) deðiþkeniyle bir bot oluþturalým ve ayrýcalýklarý ona aktaralým
-while True:
-    a  =   random.randint (1,2)
-    time.sleep(0.1)
+
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
-    from bot_mantik import gen_pass
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
 
-    intents = discord.Intents.default()
-    intents.message_content = True
+@bot.command()
+async def bye(ctx):
+    await ctx.send(f'Görüşürüz {bot.user}!')
 
-    client = discord.Client(intents=intents)
+@bot.command()
+async def nasilsin(ctx):
+    await ctx.send(f'İyiyim {bot.user}! teşekkürler')
 
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
+@bot.command()
+async def merhaba(ctx):
+    await ctx.send(f'Merhaba {bot.user}!')
 
+@bot.command()
+async def sen_bir_botsun(ctx):
+    await ctx.send(f'Evet {bot.user}! Ben bir botum!')
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-        if message.content.startswith('hello'):
-            await message.channel.send("Hi!")
-        elif message.content.startswith('bye'):
-            await message.channel.send("Keþke oyun gelsen")
-        elif message.content.startswith('nasilsin'):
-            await message.channel.send("iyiyim ya sen?")
-        elif message.content.startswith('merhaba'):
-            await message.channel.send("merhaba")
-        elif message.content.startswith('sen bir botsun'):
-            await message.channel.send("bu doðru")
-        elif message.content.startswith('sifre'):
-            await message.channel.send(gen_pass(8))
-        elif message.content.startswith('iyiyim teþekkürler'):
-            await message.channel.send('rica ederim') 
-        
-        
-        elif message.content.startswith('yazi tura'):
-            random_result = random.randint(1, 2)
-            await message.channel.send('yazi mi? tura mi?')
-        elif message.content.startswith('tura'):
-            if a == 1:
-                await message.channel.send('bildin')
-            elif a != 1:
-                await message.channel.send('bilemedin')
-        
-        
-        elif message.content.startswith('yazi'):
-            if a == 2:
-                await message.channel.send('bildin')
-            elif a != 2:
-                await message.channel.send('bilemedin')
+@bot.command()
+async def sifre(ctx):
+    await ctx.send(gen_pass(8))
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
 
 
 
+@bot.command()
+async def yazi_tura(ctx):
+    await ctx.send(f'{bot.user}! yazı mıtura mı?')
+
+@bot.command()
+async def tura(ctx):
+    if a == 1:
+        await ctx.send(f'Evet {bot.user}! Bildin!')
+    elif a != 1:
+        await ctx.send(f'Hayır {bot.user}! Bilemedin!')
     
-        """else:
-            await message.channel.send(message.content)"""
 
-    client.run("MTEzNjY5ODgwOTQ3MzMwNjcyNA.GPE-zf.DJyty2LLgIXBPRKdkqOLGlx1tP2n6oeTmexQPU")
+
+@bot.command()
+async def yazi(ctx):
+
+    if a == 2:
+        await ctx.send(f'Evet {bot.user}! Bildin!')
+    elif a != 2:
+        await ctx.send(f'Hayır {bot.user}! Bilemedin!')
+
+
+
+
+
+
+
+
+
+
+
+@bot.command()
+async def sen_bir_botsun(ctx):
+    await ctx.send(f'Evet {bot.user}! Ben bir botum!')
+
+
+bot.run("Token")
